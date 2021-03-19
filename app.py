92cf3d88b7caad7f277dad2f2dcf4a60cafaf89d
Amien <be._.positive@hotmail.com>
2021-03-19 20:27:16 +0200

    test worn congfi

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify, redirect
 from PIL import Image
 import numpy as np
 import joblib
@@ -23,22 +23,48 @@ def home():
 
 @app.route("/predict", methods=["POST"])
 def predict():
-	print(request.files)
+
+	# check if file is not in the requst
 	if 'file' not in request.files:
 		return "Error"
+
 	# Save File
 	file = request.files['file']
 	file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
 	file.save(file_path)
-	print(file_path)
+
+	# read the image and convert it to grey scale
 	img = Image.open(file_path)
 	my_img_resized = np.array(img.resize((28,28)).convert('L'))
-	print(my_img_resized)
+
+	# Predict
 	prediction = model.predict(my_img_resized.reshape(-1, 28*28)/255)
 
 	return render_template("index.html", digit=prediction[0])	
 
 
+
+
+@app.route("/api/predict", methods=["POST"])
+def api_predict():
+
+	# check if file is not in the requst
+	if 'file' not in request.files:
+		return "Error"
+
+	# Save File
+	file = request.files['file']
+	file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+	file.save(file_path)
+
+	# read the image and convert it to grey scale
+	img = Image.open(file_path)
+	my_img_resized = np.array(img.resize((28,28)).convert('L'))
+
+	# Predict
+	prediction = model.predict(my_img_resized.reshape(-1, 28*28)/255)
+
+	return jsonify({"prediction":str(prediction[0])})
 
 
 if __name__ == "__main__":
